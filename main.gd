51c2344f46308798db8ab2802c5153a14f5ac373
weak_ptr <weak_ptr@outlook.com>
2021-12-29 17:29:51 +0800

    feat(main): add bgm and death sound for main scene

--- main.gd
+++ main.gd
@@ -15,6 +15,8 @@ func game_over():
 	$MobTimer.stop()
 
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 
 func new_game():
@@ -26,6 +28,7 @@ func new_game():
 	$HUD.show_message("Get Ready")
 
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_MobTimer_timeout():
