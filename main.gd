73206af02bbf413a537de3add937432e1289e125
weak_ptr <weak_ptr@outlook.com>
2021-12-29 15:59:33 +0800

    feat(main): finish mob spawn

--- main.gd
+++ main.gd
@@ -3,27 +3,39 @@ extends Node
 export(PackedScene) var mob_scene
 var score
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	new_game()
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 
+
 func new_game():
-	score=0
-	$Player.start($StartPosition)
+	score = 0
+	$Player.start($StartPosition.position)
 	$StartTimer.start()
 
 
 func _on_MobTimer_timeout():
-	pass # Replace with function body.
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+
+	var mob = mob_scene.instance()
+	add_child(mob)
+
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	mob.position = mob_spawn_location.position
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
 
 
 func _on_ScoreTimer_timeout():
